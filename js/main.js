406fd3fb745cadcdf3ef37258823f4214e9de20d
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-08-14 00:29:37 -0400

    Added basic greensock, and styling updates

--- js/main.js
+++ js/main.js
@@ -3,17 +3,35 @@
 	"use strict";
 	console.log();
 	const TIME_TILL_POPUP = 3000;
-	const popOver = document.querySelector(".popOver");
+	const POPOVER = document.querySelector(".popOver");
+	const CHISEL_IMG = document.querySelector('.chiselImg');
+	const SHAVINGS_IMG = document.querySelector('.shavingsImg');	
 
 	// show pop-over after 3 seconds
 	window.setTimeout(() => {
-		popOver.classList.remove("hidden");
+		console.log('3')
+		POPOVER.classList.remove("hidden");
 	}, TIME_TILL_POPUP);
 
 	// close pop-over on keyboard shortcut, ESC (ascii 27)
 	document.onkeydown = function(e) {
 		if(e.keyCode == 27) {
-			popOver.classList.add("hidden");
+			POPOVER.classList.add("hidden");
 		}
 	}
+
+	// scroll Parallax
+	gsap.registerPlugin(ScrollTrigger);
+	gsap.to(CHISEL_IMG, {
+		scrollTrigger: CHISEL_IMG,
+		//  {
+			// trigger: CHISEL_IMG,
+			// toggleActions: "restart pause restart pause"
+		// },
+		// x: 10,
+		// rotation: 360,
+		y: 10,
+		duration: 5
+	});
+
 })();
